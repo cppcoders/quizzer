8460dc384ed8885176edec2974b90392baad84bd
Dodger23 <tarekhassan3874@gmail.com>
2021-07-08 06:10:51 +0200

    activated join_group, create_group

--- users/serializers.py
+++ users/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ['name']
+        fields = ['id','name']
