bfb7612637f05c94575bf6beecd6456ababa5a10
khaledunilearn <khaled@unilearn.com.eg>
2021-07-13 15:59:41 +0200

    add database configrations

--- quizzer/settings.py
+++ quizzer/settings.py
@@ -94,14 +94,23 @@ WSGI_APPLICATION = 'quizzer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd1tj0qjddnlidk',
+        'USER': 'hibnxosobijbbg',
+        'PASSWORD': '4701582d44910cee6e3de13f752830dde1e970bd269a65e6b77205c40964fe0d',
+        'HOST': 'ec2-52-50-171-4.eu-west-1.compute.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
